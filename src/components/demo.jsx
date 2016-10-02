c785f0d15d65c138ee2fdbc51d54f9db88829ef9
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2016-10-03 00:20:35 +0200

    step 5: component state and child-parent communication

--- src/components/demo.jsx
+++ src/components/demo.jsx
@@ -3,11 +3,24 @@ import Title from './title';
 import Form from './form';
 
 export default class Demo extends Component {
+  constructor () {
+    super();
+
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.state = {
+      name: 'Escuela IT'
+    };
+  }
+
+  handleNameChange (event) {
+    this.setState({ name: event.target.value });
+  }
+
   render () {
     return (
       <div className='demo'>
-        <Title name='Escuela IT' />
-        <Form />
+        <Title name={this.state.name} />
+        <Form handleNameChange={this.handleNameChange} />
       </div>
     );
   }
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 export default class Form extends Component {
+  static get propTypes () {
+    return {
+      handleNameChange: PropTypes.func.isRequired
+    };
+  }
+
   render () {
     return (
       <div className='form-field'>
         <label htmlFor='name'>Name: </label>
-        <input type='text' id='name' onChange={
-          event => console.log(event.target.value)
-        } />
+        <input type='text' id='name' onChange={this.props.handleNameChange} />
       </div>
     );
   }
