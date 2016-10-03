3c05844f18370acb1c6d0f72bfca0ef50d28239c
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2016-10-03 11:18:42 +0200

    step 7.1: improved proptype validation

--- src/components/title.jsx
+++ src/components/title.jsx
@@ -1,15 +1,14 @@
 import React, { PropTypes } from 'react';
 import { translations } from '../data';
+import { languages } from '../config';
 
 export default function Title ({ name, language }) {
-  function title () {
-    return translations[language]['TITLE'].replace('%name%', name);
-  }
+  const title = translations[language]['TITLE'].replace('%name%', name);
 
-  return <h1 className='title'>{title()}</h1>;
+  return <h1 className='title'>{title}</h1>;
 }
 
 Title.propTypes = {
   name: PropTypes.string.isRequired,
-  language: PropTypes.string.isRequired
+  language: PropTypes.oneOf(languages).isRequired
 };
