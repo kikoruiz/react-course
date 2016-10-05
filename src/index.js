172eacb3aff941f9453ded489abde590b7089751
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2016-10-06 00:50:39 +0200

    step 16.1: moved to hash history in order to make demo work in github pages

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, IndexRoute, Redirect, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute, Redirect, hashHistory } from 'react-router';
 import App from './components/app';
 import Demo from './components/demo';
 import './index.scss';
 
-const DEFAULT_LANGUAGE = 'es';
-
 render(
-  <Router history={browserHistory}>
-    <Redirect from='/react-course' to={DEFAULT_LANGUAGE} />
-    <Redirect from='/' to={DEFAULT_LANGUAGE} />
-    <Route path=':language' component={App}>
+  <Router history={hashHistory}>
+    <Redirect from='/' to='es' />
+    <Route path='/:language' component={App}>
       <IndexRoute component={Demo} />
     </Route>
   </Router>,
