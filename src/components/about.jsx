7be98b3d7e26d33c26eafe50d168e95793472797
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2016-10-06 01:10:25 +0200

    step 17: added a second route

---
+++ src/components/about.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function About () {
+  return (
+    <div>
+      <h2>About me</h2>
+      <p>Bla, bla, bla...</p>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import { Router, Route, IndexRoute, Redirect, hashHistory } from 'react-router';
 import App from './components/app';
 import Demo from './components/demo';
+import About from './components/about';
 import './index.scss';
 
 render(
@@ -10,6 +11,7 @@ render(
     <Redirect from='/' to='es' />
     <Route path='/:language' component={App}>
       <IndexRoute component={Demo} />
+      <Route path='about' component={About} />
     </Route>
   </Router>,
   document.getElementById('demo')
