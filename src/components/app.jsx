023411ab67913177e1018fa63843477a8683adca
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2016-10-06 01:03:34 +0200

    step 16.2: caught wrong language within routes

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,8 +1,14 @@
 import React, { PropTypes } from 'react';
 import { translations } from '../data';
+import { languages } from '../config';
 
 export default function App ({ children, params }) {
   const { language } = params;
+
+  if (languages.indexOf(language) === -1) {
+    return <h1>404 error</h1>;
+  }
+
   const header = translations[language].HEADER;
 
   return (
